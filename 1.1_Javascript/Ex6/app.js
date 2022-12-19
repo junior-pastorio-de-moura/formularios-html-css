fd95f422dfad9d1f45c5c48c65333237b710de62
Júnior Pastório de Moura <74156231+junior-pastorio-de-moura@users.noreply.github.com>
2022-12-18 21:00:44 -0300

    Add files via upload

---
+++ 1.1_Javascript/Ex6/app.js
@@ -0,0 +1,7 @@
+//EX: Imprimir no console somente valores ímpares
+console.log("\nVALORES ÍMPARES de 1 a 30\n");
+for (let i = 1; i <= 30; i++) {
+    if (i % 2 == 1) {
+        console.log(i+"\n");
+    }
+}
