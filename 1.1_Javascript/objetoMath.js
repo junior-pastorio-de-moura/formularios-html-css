560b0bc8dd10b82aa7f51891e39c3c219c1efa43
Júnior Pastório de Moura <74156231+junior-pastorio-de-moura@users.noreply.github.com>
2023-03-06 21:15:06 -0300

    Add files via upload

---
+++ 1.1_Javascript/objetoMath.js
@@ -0,0 +1,91 @@
+//Objeto Math - Objeto para funções matemáticas
+
+//Comprimento de uma circunferência com 30 de raio
+
+//Math.PI = Retorna o número PI
+console.log(2 * Math.PI * 30);
+
+//Math.abs - Retrona o valor absoluto de um número
+console.log(Math.abs(-3.89));
+
+//Math.max() - Retorna o número de maior valor dentro de um conjunto
+console.log(Math.max(3.3, 44, 102, 55, 338, 15, 25, 188, -2));
+
+//Math.min() - Retorna o número de menor valor
+console.log(Math.min(3.3, 44, 102, 55, 338, 15, 25, 188, -2));
+
+//Math.round() - Arredonda o valor para o número inteiro mais próximo
+console.log(Math.round(3.4));
+
+//Math.floor() - Arredonda para baixo
+console.log(Math.floor(4.9));
+
+//Math.ceil() - Arredonda para cima
+console.log(Math.ceil(7.2));
+
+//Math.pow() - Eleva algum número à uma determinada potência
+console.log("7 elevado ao quadrado: "+ Math.pow(7,2));
+console.log("2 elevado na 7: "+ Math.pow(2,7));
+
+//Math.sqrt() - Extrai a raiz quadrada de algum número
+console.log("Raiz quadrada de 25: "+ Math.sqrt(25));
+
+//Math.random() - Retorna um número randômico aleatório de zero até 1
+console.log(Math.random()*10);
+console.log(Math.round(Math.random()*10));
+
+//Math.exp() - Eleva o número de Euler à potência mencionada
+console.log(Math.exp(3));
+
+//Math.log() - Extrai o logaritmo natural do número informado
+console.log(Math.log(3));
+
+//Math.sin() - Retorna o valor do seno do ângulo mencionado
+console.log(Math.sin(90));
+
+//Math.cos() - Retorna o valor do cosseno do ângulo mencionado
+console.log(Math.cos(90));
+
+//Math.tan() - Retorna o valor da tangente do ângulo mencionado
+console.log(Math.tan(90));
+
+//Math.asin() - Retorna o valor numérico em radianos do arco
+console.log(Math.asin(90));
+
+/*Math.acos() - Retorna o valor numérico em radianos representando o arco cujo
+cosseno seja passado no parâmetro da função.
+*/
+console.log(Math.acos(90));
+
+/*Math.atan() - Retorna o valor numérico entre  -PI/2 e PI/2 representando o
+arco em radianos cuja tangente seja passado no parâmetro da função.
+*/
+console.log(Math.atan(90));
+
+/*Math.atan2() - Retorna o valor numérico entre  -PI/2 e PI/2 representando o
+arco em radianos cuja tangente seja igual ao cuociente do parâmetro da função.
+*/
+console.log(Math.atan2(1,1));
+
+/*
+RESUMO
+Math.PI
+Math.abs()
+Math.max()
+Math.min()
+Math.round()
+Math.floor()
+Math.ceil()
+Math.pow()
+Math.sqrt()
+Math.random()
+Math.exp()
+Math.log()
+Math.sin()
+Math.cos()
+Math.tan()
+Math.asin()
+Math.acos()
+Math.atan()
+Math.atan2()
+*/
