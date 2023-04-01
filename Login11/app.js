9fe517fca5ae7b178c69aaafe6d1589d2dd09dac
Júnior Pastório de Moura <74156231+junior-pastorio-de-moura@users.noreply.github.com>
2023-04-01 12:31:58 -0300

    Delete Login11 directory

--- Login11/app.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".text-input").forEach((Element) =>{
-    Element.addEventListener("blur", (event) => {
-        if (event.target != "") {
-            event.target.nextElementSibling.classList.add("filled");
-        } else {
-            event.target.nextElementSibling.classList.remove("filled")
-        }
-    });
-});
