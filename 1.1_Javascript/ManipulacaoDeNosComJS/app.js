e8fdb7cd34dafc03dd8a06977b52a4365df0c174
Júnior Pastório de Moura <74156231+junior-pastorio-de-moura@users.noreply.github.com>
2023-04-10 18:11:16 -0300

    Add files via upload

---
+++ 1.1_Javascript/ManipulacaoDeNosComJS/app.js
@@ -0,0 +1,80 @@
+/* MANIPULAÇÃO DE NÓS
+
+CRIAÇÃO DE NÓS:
+    createElement
+    createTextNode
+    createAttribute
+
+ATRIBUIÇÃO DE NÓS
+    appendChild
+    removeChild
+    repleaceChild
+*/
+
+
+window.onload = function () {
+    var div = document.getElementsByTagName("div").item(0);
+
+    //Criandao um elemento
+    var hr = document.createElement("hr");
+    //Adicionando esse elemento (hr) no div
+    div.appendChild(hr);
+
+    var p = document.createElement("p");
+    div.appendChild(p);
+    p.innerHTML = "algo escrito em um parágrafo criado com javascript, com um texto adicionado com javascript";
+
+    var li = document.createElement("li");
+    div.appendChild(li);
+    li.innerHTML = "Um ítem de lísta";
+
+    //Criando um nó do tipo atributo
+    var atributo = document.createAttribute("style");
+    //Adicionando um valor a esse atributo
+    atributo.value = "color:red";
+    //Associando esse atributo à algum elemento
+    li.setAttributeNode(atributo);
+
+    //O código da linha abaixo sobrescreve o da linha 34
+    li.setAttribute("style", "font-size: 35px; color: green");
+
+    //A linha de código abaixo sobrepõe a da linha 39
+    li.style.color = "blue";
+
+    //Criando um campo do tipo select
+    var select = document.createElement("select");
+    select.setAttribute("id", "estados");
+    //Colocando um "option" dentro do "select"
+    var option = document.createElement("option");
+    option.setAttribute("value", "DF");
+    option.appendChild(document.createTextNode("Distrito Federal"));
+    select.appendChild(option);
+    div.appendChild(select);
+
+    var option2 = document.createElement("option");
+    option2.setAttribute("value", "RS");
+    option2.appendChild(document.createTextNode("Rio grande do sul"));
+    select.appendChild(option2);
+
+    //Clonando elementos
+    var select2 = select.cloneNode(true);
+    div.appendChild(select2);
+
+    //Clonando uma opção e jogando ela para dentro do primeiro select
+    var option3 = option.cloneNode(true);
+    select.appendChild(option3);
+
+}
+
+ /*ESSA BOSTA AQUI NÃO DEU CERTO
+    //Criando um novo item de lista para adicionar na lista existente
+    var li = document.createElement("li");
+    //li.appendChild(document.createTextNode("teste de lista"));
+    li.innerHTML = "teste de lista";
+    //Adiciona mais um elemento dentro do primeiro filho da div
+    div.firstChild.appendChild(li);
+
+    //REMOVENDO ELEMENTOS
+    var del = div.firstChild;
+    div.removeChild(del);
+    */
